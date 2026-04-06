a4719c5b12dcc2600e3d455f3aced943617814fd
Jhonatan Julian Comezaquira Ochoa <comezaquirajhonatan@gmail.com>
2026-04-06 06:32:17 -0500

    Update Productocontroller.js

--- backend/controller/Productocontroller.js
+++ backend/controller/Productocontroller.js
@@ -1,1 +1,15 @@
+const Categoria = require('../models/CategoriaModel');
 
+exports.obtenerCategorias = (req, res) => {
+    Categoria.getAll((err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.json(data);
+    });
+};
+
+exports.crearCategoria = (req, res) => {
+    Categoria.create(req.body, (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.json({ message: "Categoría creada con éxito" });
+    });
+};
