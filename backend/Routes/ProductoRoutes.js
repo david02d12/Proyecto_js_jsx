68894c583640a1eaaf0a734c0f21ebb3c39dfb2c
Jhonatan Julian Comezaquira Ochoa <comezaquirajhonatan@gmail.com>
2026-04-06 07:09:20 -0500

    Update ProductoRoutes.js

--- backend/Routes/ProductoRoutes.js
+++ backend/Routes/ProductoRoutes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const ProductoController = require('../controllers/ProductoController');
 
+router.get('/', ProductoController.listarProducto);
+router.get('/:id', ProductoController.obtenerProducto);
+router.post('/', ProductoController.crearProducto);
+router.put('/:id', ProductoController.actualizarProducto);
+router.delete('/:id', ProductoController.eliminarProducto); 
+
+module.exports = router;
