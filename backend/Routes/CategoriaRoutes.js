47fb929999d6032b746dd8da39eae20d8527d3ca
Jhonatan Julian Comezaquira Ochoa <comezaquirajhonatan@gmail.com>
2026-04-09 07:36:16 -0500

    Delete backend/Routes/CategoriaRoutes.js

--- backend/Routes/CategoriaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoriaController = require('../controllers/CategoriaController');
-
-router.get('/', categoriaController.obtenerCategorias);
-router.post('/', categoriaController.crearCategoria);
-
-module.exports = router;
