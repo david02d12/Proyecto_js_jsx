f6f24ddd04170927a594ba42f9646872efbf22d6
Jhonatan Julian Comezaquira Ochoa <comezaquirajhonatan@gmail.com>
2026-04-09 06:45:59 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const mysql = require('mysql2');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocumentation = require('./swagger.json');
 const cors = require('cors');
+
 const app = express();
 
-app.use(cors());
+// Middlewares
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
+// Swagger
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocumentation));
+
+// Conexión a la base de datos
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '', 
-    database: 'celuaccel' 
+    password: '',
+    database: 'celuaccel'
 });
 
 db.connect(err => {
@@ -18,13 +26,16 @@ db.connect(err => {
         console.error('Error conectando a la DB:', err);
         return;
     }
-    console.log(' Conectado a la base de datos CeluAccel');
+    console.log('Conectado a la base de datos celuaccel');
 });
 
 global.db = db;
 
-const productoRoutes = require('./routes/ProductoRoutes');
-app.use('/productos', productoRoutes);
+// Importar todas las rutas centralizadas
+const routes = require('./routes/Routes');
+app.use('/api', routes);
+
+// Arrancar servidor
 app.listen(3000, () => {
-    console.log(" Servidor corriendo en puerto 3000");
+    console.log("Servidor corriendo en puerto 3000");
 });
