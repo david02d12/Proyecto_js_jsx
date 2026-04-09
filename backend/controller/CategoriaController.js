fad13a50034165c1f851dd08e59808b49c02c214
Jhonatan Julian Comezaquira Ochoa <comezaquirajhonatan@gmail.com>
2026-04-09 09:38:03 -0500

    Update CategoriaController.js

--- backend/controller/CategoriaController.js
+++ backend/controller/CategoriaController.js
@@ -1,15 +1,31 @@
-const Categoria = require('../models/CategoriaModel');
+const db = require('../config/db');
 
-exports.obtenerCategorias = (req, res) => {
-    Categoria.getAll((err, data) => {
+exports.listar = (req, res) => {
+    db.query('SELECT * FROM Categoria', (err, results) => {
         if (err) return res.status(500).json(err);
-        return res.json(data);
+        res.json(results);
     });
 };
 
-exports.crearCategoria = (req, res) => {
-    Categoria.create(req.body, (err, data) => {
+exports.agregar = (req, res) => {
+    const { ID_Categoria, Nombre_Categoria } = req.body;
+    db.query('INSERT INTO Categoria VALUES (?, ?)', [ID_Categoria, Nombre_Categoria], (err) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.status(201).json({ message: "Categoría creada" });
+    });
+};
+
+exports.actualizar = (req, res) => {
+    const { Nombre_Categoria, ID_Categoria } = req.body;
+    db.query('UPDATE Categoria SET Nombre_Categoria=? WHERE ID_Categoria=?', [Nombre_Categoria, ID_Categoria], (err) => {
         if (err) return res.status(500).json(err);
-        return res.json({ message: "Categoría creada con éxito" });
+        res.json({ message: "Categoría actualizada" });
+    });
+};
+
+exports.eliminar = (req, res) => {
+    db.query('DELETE FROM Categoria WHERE ID_Categoria = ?', [req.params.id], (err) => {
+        if (err) return res.status(500).json(err);
+        res.json({ message: "Categoría eliminada" });
     });
 };
